cde3ae801f1d3ccc4ada8bb9231cde89caafa3b8
deni-rhn <denirhmnt@gmail.com>
2019-09-21 14:10:05 +0700

    angular x mongodb

---
+++ src/app/gst-get/gst-get.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { BusinessService } from '../business.service';
+import Business from '../Business_model';
+
+@Component({
+  selector: 'app-gst-get',
+  templateUrl: './gst-get.component.html'
+})
+export class GstGetComponent implements OnInit {
+  businesses: Business[];
+  constructor(private bs: BusinessService) { }
+
+  deleteBusiness(id) {
+    this.bs.deleteBusiness(id).subscribe(res => {
+      console.log('Deleted');
+      this.ngOnInit();
+    });
+  }
+
+  refresh() {
+    location.reload();
+  }
+  ngOnInit() {
+    this.bs.getBusinesses().subscribe((data: Business[]) => {
+      this.businesses = data;
+    });
+  }
+
+}
